f5a9430ca2089a4602c0e0cfa6096f99e3e6427a
Web-Dev-Jr <samkautz@yahoo.com>
2021-06-13 21:30:19 -0500

    Update Contact page Grid styling. Remove Contact page Container

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -23,75 +23,70 @@ export default function Contact() {
           </Typography>
         </Grid>
       </Grid>
-      <Container className={classes.contactHero} maxWidth="false">
-        {/* TOP CONTACT GRID WITH CONTACT INFO */}
-        <Grid
-          container
-          spacing={10}
-          style={{
-            paddingTop: "100px",
-          }}
-        >
-          <Grid item lg={6} md={6} xs={12}>
-            <Typography variant="h4" align="center">
-              David McKenzie
-            </Typography>
-            <hr />
-            <Typography variant="subtitle1" align="center">
-              Partner
-            </Typography>
-            <Typography variant="subtitle1" align="center">
-              David@SNAXMEDIA.COM
-            </Typography>
-          </Grid>
-          <Grid item lg={6} md={6} xs={12}>
-            <Typography variant="h4" align="center">
-              Scott Donald
-            </Typography>
-            <hr />
-            <Typography variant="subtitle1" align="center">
-              Partner
-            </Typography>
-            <Typography variant="subtitle1" align="center">
-              SCOTT@SNAXMEDIA.COM
-            </Typography>
-          </Grid>
+      {/* TOP CONTACT GRID WITH CONTACT INFO */}
+      <Grid
+        container
+        spacing={10}
+        style={{
+          padding: "70px 100px 0 100px",
+        }}
+      >
+        <Grid item lg={6} md={6} xs={12}>
+          <Typography variant="h4" align="center">
+            David McKenzie
+          </Typography>
+          <hr />
+          <Typography variant="subtitle1" align="center">
+            Partner
+          </Typography>
+          <Typography variant="subtitle1" align="center">
+            David@SNAXMEDIA.COM
+          </Typography>
         </Grid>
-        {/* BOTTOM CONTACT GRID WITH CONTACT FORM AND MAP */}
-        <Grid
-          container
-          spacing={10}
-          style={{
-            padding: "100px 0 50px 0",
-          }}
-        >
-          <Grid item xs={6}>
-            <FormGroup
-              column
-              className={classes.root}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField id="filled-basic" label="Name" variant="filled" />
-              <TextField id="filled-basic" label="Email" variant="filled" />
-              <TextField
-                id="filled-basic"
-                label="Phone Number"
-                variant="filled"
-              />
-              <TextField
-                id="filled-basic"
-                label="Web Address"
-                variant="filled"
-              />
-              <TextField id="filled-basic" label="Message" variant="filled" />
-            </FormGroup>
-          </Grid>
-          <Grid item lg={6} md={6} xs={12}>
+        <Grid item lg={6} md={6} xs={12}>
+          <Typography variant="h4" align="center">
+            Scott Donald
+          </Typography>
+          <hr />
+          <Typography variant="subtitle1" align="center">
+            Partner
+          </Typography>
+          <Typography variant="subtitle1" align="center">
+            SCOTT@SNAXMEDIA.COM
+          </Typography>
+        </Grid>
+      </Grid>
+      {/* BOTTOM CONTACT GRID WITH CONTACT FORM AND MAP */}
+      <Grid
+        container
+        spacing={10}
+        style={{
+          padding: "100px 100px 50px 100px",
+        }}
+      >
+        <Grid item lg={6} md={6} xs={12}>
+          <FormGroup
+            column
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="filled-basic" label="Name" variant="filled" />
+            <TextField id="filled-basic" label="Email" variant="filled" />
+            <TextField
+              id="filled-basic"
+              label="Phone Number"
+              variant="filled"
+            />
+            <TextField id="filled-basic" label="Web Address" variant="filled" />
+            <TextField id="filled-basic" label="Message" variant="filled" />
             <MyButton></MyButton>
-          </Grid>
+          </FormGroup>
         </Grid>
-      </Container>
+        <Grid item lg={6} md={6} xs={12}>
+          <MyButton></MyButton>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
